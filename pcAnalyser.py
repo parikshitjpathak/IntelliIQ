9437bc795c5d353517e4a6a1e22b72cc5be5d616
parikshitjpathak <parikshitjpathak@gmail.com>
2026-04-27 16:52:24 +0530

    Update pcAnalyser.py

--- pcAnalyser.py
+++ pcAnalyser.py
@@ -1733,5 +1733,5 @@ def create_confluence():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,use_reloader=False)
-
+      port = int(os.environ.get("PORT", 10000))
+      app.run(host="0.0.0.0", port=port)
